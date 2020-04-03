12e479bc295cf1a1a4e7e8fe135bcdb6a6d7c185
Ashim Sedhain <asedhain@login3.stampede2.tacc.utexas.edu>
2020-04-03 16:33:48 -0500

    added init.sh script for installing all the packages for stampede2

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module load intel/18.0.2 python3/3.7.0
+pip3 install --user /home1/apps/tensorflow/builds/intel-18.0.2/tensorflow-1.13.1-cp37-cp37m-linux_x86_64.whl
+pip3 install --user keras
+pip3 install --user --force-reinstall h5py --no-deps
+export PYTHONPATH=$HOME/.local/lib/python3.7/site-packages:/opt/apps/intel18/impi18_0/python3/3.7.0/lib/python3.7/site-packages
+module load boost/1.68
+CPLUS_INCLUDE_PATH=/opt/apps/intel18/boost/1.68/include HOROVOD_WITH_TENSORFLOW=1 \
+              HOROVOD_WITHOUT_PYTORCH=1 HOROVOD_WITHOUT_MXNET=1 pip3 install \
+              --user horovod==0.16.4 --no-cache-dir
+
