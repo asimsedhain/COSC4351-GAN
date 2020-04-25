6ec764c8aa9b8f3fd8be13b6002b41fd8c24d6bc
Ashim Sedhain <asimsedhain33@gmail.com>
2020-04-25 01:59:42 -0500

    reverted back

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import numpy as np
 
 
 
-data = tf.data.Dataset.from_tensor_slices([0, 1, 2, 3, 4, 5, 6, 7]).shuffle(8).batch(2)
-
-for i in range(2):
-	for j in data:
-		print(j.numpy())
+data = tf.data.Dataset.from_tensor_slices([0, 1, 2, 3, 4, 5, 6, 7]).shuffle(8).batch(2).repeat()
+for j in range(10):
+	for i in data.take(2):
+		print(i.numpy())
